84283d74464cb02f59a1ca2786586d62fd516347
Uriel Hass <uriel@facedigital.com.br>
2021-10-19 00:09:51 -0300

    Dia 1

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastMessagesController } from "./controllers/GetLastMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { EnsureAuthenticated } from "./middleware/EnsureAuthenticated";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", EnsureAuthenticated, new CreateMessageController().handle);
+
+router.get("/messages/last", EnsureAuthenticated, new GetLastMessagesController().handle);
+
+router.get("/profile", EnsureAuthenticated, new ProfileUserController().handle);
+
+export { router }
